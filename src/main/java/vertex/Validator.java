0530ee57a7e6ac94033797424fa6a5aff48f8213
roman.devyatov <roman.devyatov@stm-labs.ru>
2021-06-05 19:59:37 +0300

    ref

--- src/main/java/vertex/Validator.java
+++ src/main/java/vertex/Validator.java
@@ -12,19 +12,16 @@ public class Validator<F> {
         return checkIfVertexIsNotNull(vertex) && !vertexList.contains(vertex);
     }
 
-    protected boolean checkIfEdgeIsNull(Edge<F> e) {
-        return e == null || e.getVertex() == null || e.getVertexAnother() == null;
+    protected boolean checkIfEdgeIsNotNull(Edge<F> e) {
+        return e != null && e.getVertex() != null && e.getVertexAnother() != null;
     }
 
     protected boolean checkAddEdge(List<Edge<F>> list, Edge<F> e) {
-        return !checkIfEdgeIsNull(e) && !list.contains(e);
+        return checkIfEdgeIsNotNull(e) && !list.contains(e);
     }
 
     protected boolean checkDeleteEdge(List<Edge<F>> edges, Edge<F> e) {
-        if (e != null && e.getVertexAnother() != null && e.getVertex() != null) {
-            return edges.contains(e);
-        }
-        return false;
+        return checkIfEdgeIsNotNull(e) && edges.contains(e);
     }
 
     protected boolean checkDeleteVertex(Vertex<F> v) {
